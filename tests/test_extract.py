227933905e7969dcab6782e01750606e4814f719
BaddireddyManikantha <baddireddymani0@gmail.com>
2025-07-09 14:16:29 +0530

    code added into text_extract.py

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -1,4 +1,9 @@
+
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from etl.extract import extract_data
+
 
 def test_extract_data():
     df = extract_data("https://jsonplaceholder.typicode.com/users")
