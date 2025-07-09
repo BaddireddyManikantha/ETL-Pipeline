d12931dc59ca9dd728f57de265a0a948bd75996c
BaddireddyManikantha <baddireddymani0@gmail.com>
2025-07-09 14:28:16 +0530

    code change in test scipts

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -5,9 +5,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from etl.extract import extract_data
 from etl.logger import get_logger
 
-
 def test_extract_data():
-    logger = get_logger("test_extract_data")  # initialize logger
+    logger = get_logger("test_extract_data", "test_etl.log")  # added log_file argument
 
     df = extract_data("https://jsonplaceholder.typicode.com/users", logger)
 
--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,7 +1,16 @@
-import pandas as pd
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from etl.transform import transform_data
+from etl.logger import get_logger
+import pandas as pd
 
 def test_transform_data():
+    logger = get_logger("test_transform_data", "test_etl.log")
+
     df = pd.DataFrame({"ID": [1, 2], "Name": ["A", "B"]})
-    clean_df = transform_data(df,logger)
+    clean_df = transform_data(df, logger)
+
+    assert not clean_df.empty
     assert "id" in clean_df.columns
